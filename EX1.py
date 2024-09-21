83c5f06b23f9ad989f90ee16b705a3a77478613d
Harshu282001 <108525411+Harshu282001@users.noreply.github.com>
2024-09-21 20:10:02 +0530

    Add files via upload

---
+++ EX1.py
@@ -0,0 +1,6 @@
+import cv2
+img=cv2.imread("sample1.png")
+cv2.imshow("Iron man",img)
+cv2.imwrite("Ironman.png".img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ EX2.py
@@ -0,0 +1,5 @@
+import cv2
+img=cv2.imread("sample1.png")
+print(img.shape)
+print(img.size)
+print(img.dtype)
